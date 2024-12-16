3b526018be96c33e3ba2ef3c6a946d6f2fdfec76
Mikalai <128977965+FangelGit@users.noreply.github.com>
2024-12-16 15:21:34 +0100

    Add GUI

--- src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
+++ src/main/java/pl/put/poznan/transformer/rest/TextTransformerController.java
@@ -3,24 +3,21 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import pl.put.poznan.transformer.logic.JsonParser;
 import pl.put.poznan.transformer.logic.TextTransformer;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Map;
-
-import static java.awt.SystemColor.text;
 
 
 @RestController
-@RequestMapping("/transform")
+@RequestMapping("/")
 public class TextTransformerController {
 
     private static final Logger logger = LoggerFactory.getLogger(TextTransformerController.class);
 
-    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    @GetMapping(value = "/transform", produces = "application/json")
     public String get(@RequestParam(value = "text", defaultValue = "LoReM IpSuM!") String text,
                       @RequestParam(value="transforms", defaultValue="upper,reverse") String[] transforms) throws JsonProcessingException {
         JsonParser parser = new JsonParser();
@@ -33,20 +30,8 @@ public class TextTransformerController {
         return parser.ResToJson(transformer.transform(text));
     }
 
-/*    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public String post(@RequestParam(value = "text", defaultValue = "LoReM IpSuM!") String text,
-                       @RequestBody String[] transforms) {
 
-        // log the parameters
-        logger.debug(text);
-        logger.debug(Arrays.toString(transforms));
-
-        // perform the transformation, you should run your logic here, below is just a silly example
-        TextTransformer transformer = new TextTransformer(transforms);
-        return transformer.transform(text);
-    }
-*/
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    @PostMapping(value="/transform", produces = "application/json")
     public String post(@RequestBody String jsonBody) throws JsonProcessingException {
 
         JsonParser parser = new JsonParser();
@@ -65,6 +50,22 @@ public class TextTransformerController {
     }
 
 
+    @GetMapping("/")
+    public ModelAndView indexGet() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+
+    @PostMapping("/")
+    public ModelAndView indexPost(String text, String[] transforms) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("processedText", text.toUpperCase());
+        modelAndView.addObject("transforms", String.join(", ", transforms));
+        modelAndView.setViewName("success");
+        return modelAndView;
+    }
 
 }
 
