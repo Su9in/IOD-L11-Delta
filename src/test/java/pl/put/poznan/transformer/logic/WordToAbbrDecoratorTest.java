5b388c2fdff8bd7b53811b8184a5bce75ac26a30
Aldris <33839068+ItsAldris@users.noreply.github.com>
2025-01-29 15:21:59 +0100

    Minor change

--- src/test/java/pl/put/poznan/transformer/logic/WordToAbbrDecoratorTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/WordToAbbrDecoratorTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
+/*
+    Class for testing WordToAbbrDecorator class using Mock objects
+ */
 public class WordToAbbrDecoratorTest {
 
     TextContainer container;
